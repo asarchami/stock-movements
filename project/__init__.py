f29d3d199da263a8cdd13e333e5117307eb91d18
Ali Sarchami <ali.sarchami@gmail.com>
2016-12-28 01:06:46 -0600

    import user authentication

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,27 @@
+########################
+#        imports       #
+########################
 from flask import Flask
-from project.home.views import home_blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
+
+########################
+#        config        #
+########################
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config.from_object(os.environ["APP_SETTINGS"])
+db = SQLAlchemy(app)
+
+from project.users.views import users_blueprint
+from project.home.views import home_blueprint
 
 # register our blueprints
-# app.register_blueprint(users_blueprint)
+app.register_blueprint(users_blueprint)
 # app.register_blueprint(blog_blueprint)
 app.register_blueprint(home_blueprint)
---
+++ project/models.py
@@ -0,0 +1,57 @@
+from project import db  # pragma: no cover
+from project import bcrypt  # pragma: no cover
+from sqlalchemy import ForeignKey  # pragma: no cover
+from sqlalchemy.orm import relationship  # pragma: no cover
+
+
+class BlogPost(db.Model):
+    """Blog data"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
+
+    def __init__(self, title, description, author_id):
+        """docstring for __init__"""
+        self.title = title
+        self.description = description
+        self.author_id = author_id
+
+    def __repr__(self):
+        """docstring for __repr"""
+        return '<title: {}>'.format(self.title)
+
+
+class User(db.Model):
+    """create user"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship("BlogPost", backref="author")
+    # favorite_color = db.Column(db.String)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __init__(self, name, email, password):
+        self.name, self.email = name, email
+        self.password = bcrypt.generate_password_hash(password)
+
+    def __repr__(self):
+        return '<name: {}>'.format(self.name)
